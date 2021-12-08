8d347978ca6632d676c66868e101cccc93360632
kasthuribai <kasthuribai.v@gmail.com>
2021-12-08 17:04:58 -0600

    push

--- models/app.py
+++ models/app.py
@@ -1,22 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 26 12:00:57 2021
+Created on Thu Nov 25 22:18:53 2021
 
 @author: USER
 """
 
-from flask import Flask, render_template, request
-#from joblib import load
-import pickle
-import os
+from flask import Flask, jsonify, request
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+#from sklearn.externals import joblib
+import joblib
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+nltk.download('wordnet')
+nltk.download('punkt')
+from nltk.stem import WordNetLemmatizer
+import flask
+    
+###################################################
+def pre_processing(text):
+    lemmatizer = WordNetLemmatizer()
+    text = text.lower()
+    text = re.sub('[0-9]+','num',text)
+    word_list = nltk.word_tokenize(text)
+    word_list =  [lemmatizer.lemmatize(item) for item in word_list]
+    return ' '.join(word_list)
+###################################################
+
 
 app = Flask(__name__)
 clf = pickle.load(open('clf.pkl','rb'))
 loaded_vec =pickle.load(open("count_vect.pkl","rb"))
 
-
+@app.route('/')
 def symptom():
-    return render_template('../template/index.html')
+    return flask.render_template('index.html')
 
 
 @app.route('/result',methods =['POST','GET'])

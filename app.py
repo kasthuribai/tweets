13b39031cc8ce66f1a46765f0d23fafe554eb874
kasthuribai <kasthuribai.v@gmail.com>
2021-12-08 13:53:27 -0600

    procfile

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ if __name__ == '__main__':
      port = int(os.environ.get('PORT', 5000))
      #app.debug = True
      #app.run(host='192.168.5.1', port=port)
-     app.run(host='localhost', port=port,use_reloader=False, debug=True)
+     app.run(host='0.0.0.0', port=port,use_reloader=False, debug=True)
      #app.run
